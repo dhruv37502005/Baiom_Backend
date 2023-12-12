f5e8e53f9a5f7404e1cd75b8fa05c51762ec7db6
bishal_kumar <bishalkumar2001@gmail.com>
2023-12-12 16:27:46 +0530

    admin_panell setup done

--- baoiam/settings.py
+++ baoiam/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    # Custon Apps
+    # Custom Apps
     'core',
 ]
 
@@ -134,3 +135,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    "site_header": "Baoiam",
+    "site_brand": "Baoiam",
+    #"site_logo": "logo.png",
+    "copyright": "Baoiam.com",
+}
