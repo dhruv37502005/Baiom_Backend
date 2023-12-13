c742c96018edba8e696e67147642d2bd912265a1
bishal_kumar <bishalkumar2001@gmail.com>
2023-12-13 15:42:08 +0530

    ADMIN_UI_UPDATES

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-#from django.contrib import admin
 from . models import User
-admin.site.register(User)
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+  list_display = ['username', 'email', 'bio']
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+
--- userauths/models.py
+++ userauths/models.py
@@ -1,17 +1,13 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-  email = models.EmailField(unique=True)
-  username = models.CharField(max_length=50, unique=True)
-  
-  USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = ['username']
-  
-  def _str_(self):
-    return self.username
-  
-    
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    bio = models.CharField(max_length=100, blank=True)
 
-# Create your models here.
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
