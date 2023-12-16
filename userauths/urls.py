5aa382d27b19ae6d04258d601eb8e23ed6702a90
bishal_kumar <bishalkumar2001@gmail.com>
2023-12-16 16:04:33 +0530

    login_and_logout_ feature_only _logic_updated.

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from userauths.views import signup,login_view
+from userauths.views import signup, login_view, logout_view
 
 
 
 app_name = 'userauths'
 
 urlpatterns = [
-    path("signup/", signup,name='signup'),
-    path('login/',login_view,name='login'),
+    path("signup/", signup, name='signup'),
+    path("login/", login_view, name='login'),
+    path("logout/", logout_view, name='logout'),
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 from userauths.form import UseRegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 def signup(request):
     if request.method == 'POST':
@@ -10,13 +13,21 @@ def signup(request):
             new_user = form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Hey {username}, your account was created successfully.")
-            new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'],
+            
+
+            authenticated_user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
             )
-            login(request, new_user)
-            return redirect("core:index")
+
+            if authenticated_user is not None:
+                login(request, authenticated_user)
+                return redirect("core:index")
+            else:
+                # Handle authentication failure, e.g., display an error message
+                messages.error(request, "Authentication failed.")
+
     else:
-        print("user cannot be registered")
         form = UseRegisterForm()
 
     context = {
@@ -24,8 +35,42 @@ def signup(request):
     }
     return render(request, 'signup.html', context)
 
-# Rename the login function to login_view to avoid conflicts
+
 def login_view(request):
-    return render(request, 'login.html')
+    if request.user.is_authenticated:
+        messages.warning(request, f"You are already logged in as {request.user.username}")
+        return redirect("core:index")
+    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(email=email)
+            
+        except:
+            messages.warning(request, f"User with {email} does not exist")
+        
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Welcome back {user.username}")
+            return redirect("core:index")
+        else:
+            messages.warning(request, f"User does not exist, create an account.")
+    
+    context = {
+        'title': 'Login',
+    }
+    
+    return render(request, 'login.html', context)
+            
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    
+    return redirect("core:index")
 
 
