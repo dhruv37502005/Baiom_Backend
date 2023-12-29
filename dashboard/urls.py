423690380643003961b86ed5e7cf180155d59daf
ayushi jain <ayushijain.jain12345@gmail.com>
2023-12-29 17:12:42 +0530

    error please resolve anyone

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dashboard.views import dashboard
+from dashboard.views import dashboard, update_user_details
 
 
 
@@ -7,5 +7,6 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path("dashboard/", dashboard, name='dashboard'),
+    path("updateuser/", update_user_details, name='updateuser'),
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from  .forms import UserUpdateForm
+from django.contrib import messages
+from . models import DashboardUser
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def update_user_details(request):
+    if request.method=="POST":
+        form  = UserUpdateForm(request.POST, instance =  request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "SUccess")
+            return redirect(dashboard)
+    else:
+        form = UserUpdateForm()
+    
+    return render(request, "dashboard222.html",{'form':form})
+        
 
 # Create your views here.
