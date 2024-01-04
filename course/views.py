8226af679e978b0e57767d949b1f6bf54b43a021
dhruv37502005 <149358164+dhruv37502005@users.noreply.github.com>
2024-01-04 14:35:08 +0530

    Coures Display in Course Category

--- course/views.py
+++ course/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from .models import Course
+
 # Create your views here.
 def webdevelopment(request):
-    return render(request,'webdevelopment.html',{'is_webdevelopment_page': True})
+    # courses = Course.objects.all()
+    web_dev_courses = Course.objects.filter(category='Web Development', status='active')
+    return render(request, 'webdevelopment.html', {'is_webdevelopment_page': True, 'web_dev_courses': web_dev_courses})
+
+# def datascience(request):
+#     data_science_courses = Course.objects.filter(category='Data Science', status='active')
+#     return render(request, 'webdevelopment.html', {'is_webdevelopment_page': True, 'web_dev_courses': web_dev_courses})
+
+
+
+# def courses(request):
+#     course = Course.objects.all()
+#     context={
+#         'course': course,
+#     }
+#     print(course)
+#     return render(request, 'webdevelopment.html', context)
