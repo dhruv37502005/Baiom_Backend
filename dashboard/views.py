b33ec4aa0eacedfdf8a6f049fad8026413c292dd
dhruv37502005 <149358164+dhruv37502005@users.noreply.github.com>
2024-01-06 17:17:59 +0530

    Profile Form added and Dashboard url fixed

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,42 +1,31 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
+from course.models import Course
 from userauths.models import Dashboard_User
 from django.contrib.auth.models import User , auth
+from django.contrib.auth.decorators import login_required
 
 
-
+@login_required(login_url='/userauths/login/')
 def user_ui(request):
-
     if request.method == 'GET':
-
       if request.user.is_authenticated:
-
         username = request.session['username']
         user = User.objects.get(username=username)
         dash_user=Dashboard_User.objects.get(user_id=user.id)
-
-
-        return render(request,'dashboard.html' , {"user":user})
-
+        all_courses = Course.objects.all()
+        return render(request, 'dashboard.html', {"user": user, "dash_user": dash_user, 'all_courses': all_courses})
       else :
         return redirect('userauths:login')
-
-
-
     if request.method == 'POST':
-
        return render(request,'profile.html')
     
 
 def admin_ui(request):
     if request.method == 'GET':
-
       if request.user.is_authenticated:
-
         auser = request.user
-
         return redirect('/admin')
-
       else :
         return redirect('core:index')
 
