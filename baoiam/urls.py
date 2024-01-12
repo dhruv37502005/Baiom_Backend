cf0545bf94d4724ddebee7452900f823fcc5dfe9
dhruv37502005 <149358164+dhruv37502005@users.noreply.github.com>
2024-01-12 15:42:41 +0530

    Maintenance break feature implemented

--- baoiam/urls.py
+++ baoiam/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import maintenance_page, locked_page
 from userauths import views
 from dashboard import views
 from course import views
@@ -15,6 +16,9 @@ urlpatterns = [
     path('contactus/', include('contactapp.urls')),
     path('webdevelopment/', include('course.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('maintenance/', maintenance_page, name='maintenance'),
+    # path('locked/', locked_page, name='locked'),
+    # path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
 ]
 
 
--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MaintenancePage(models.Model):
+    name = models.CharField(max_length=200,unique=True)
+    is_enabled = models.BooleanField(default=False)
+    access_code = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return str(self.name)
+    
+    class Meta:
+        ordering = ['name']
