5b634605679cc848c5b930fc2c3c22317349c428
anandgin2022@gmail.com <anandgin2022@gmail.com>
2024-01-13 18:50:24 +0530

    video added in dashboard course

--- course/models.py
+++ course/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils.text import slugify
 from django.utils import timezone
-
+import cv2
+import datetime
 # Create your models here.
 
 # class Author(models.Model):
@@ -64,5 +65,43 @@ class Course(models.Model):
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
+
+
+  #  def save(self, *args, **kwargs):
+        # Call the function to calculate and update the duration
+   
+   #     self.calculate_and_update_duration()
+    #    super().save(*args, **kwargs)
+    
+
+    #def calculate_and_update_duration(self):
+        # Get the video file path
+     #   video_path = self.video.path
+
+        # Open the video file
+      #  cap = cv2.VideoCapture(video_path)
+
+        # Get the total number of frames and frames per second
+       # total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+       # fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+        # Calculate the duration in seconds
+      #  try: 
+       #     duration_seconds = total_frames / fps
+       # except: ZeroDivisionError
+
+        # Convert seconds to a timedelta object
+        #duration_timedelta = datetime.timedelta(seconds=duration_seconds)
+
+        # Update the duration field in the model
+   #     self.duration_field = duration_timedelta
+    
+    
     def __str__(self):
-        return self.title
+       return self.title
+
+
+
+  
+
+
