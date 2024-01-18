e1c45bf145c2c0b8aa72a0631a8752328ece0532
kritirajmaharana <124510753+kritirajmaharana@users.noreply.github.com>
2024-01-18 17:10:24 +0530

    signup and login issue fixed and authenticaiton sytling addded

---
+++ static/authentication/js/app.js
@@ -0,0 +1,69 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+// function delay(n){
+//   n = n | 2000;
+//   return new Promise((done) => {
+//       setTimeout(() => {
+//           done();
+//       },n);
+//   });
+// }
+// function pageTransiston() {
+//   var tl = gsap.timeline();
+//   tl.to(".loading-screen", {
+//       duration: 1.2,
+//       width: "100%",
+//       left: "0%",
+//       ease: "Expo.easeInOut"
+//   });
+//   tl.to(".loading-screen", {
+//       duration: 1,
+//       width: "100%",
+//       left: "100%",
+//       ease: "Expo.easeInOut",
+//       delay: 0.3
+//   });
+//   tl.set(".loading-screen", {
+//       left: "-100%"
+//   })
+// }
+
+// function contentAnimation(){
+//   var tl = gsap.timeline();
+//   tl.from(".animate-this", {
+//       duration: 1, y:30, opacity: 0, stagger: 0.4, delay: 0.2
+//   });
+// }
+
+// $(function() {
+//   barba.init({
+//       sync: true,
+//       transitions: [
+//           {
+//               async leave(data) {
+//                   const done = this.async();
+
+//                   pageTransiston();
+//                   await delay(1000);
+//                   done();
+//               },
+//               async enter(data){
+//                   contentAnimation();
+//               },
+//               async once(data) {
+//                   contentAnimation();
+//               },
+
+//           },
+//       ],
+//   });
+// });
