09b8fde040fac5f57d3c9bd8a1ee17f6267e563f
dhruv37502005 <149358164+dhruv37502005@users.noreply.github.com>
2024-02-13 12:29:29 +0000

    correction on purchase

--- dashboard/views.py
+++ dashboard/views.py
@@ -34,7 +34,7 @@ def user_ui(request):
                 dash_user = Dashboard_User.objects.get(user_id=user.id)
                 # get active courses if enrolled
                 # enrolled_courses = dash_user.enrolled_courses.filter(status="active")
-                purchase_courses = PurchaseCourse.objects.filter(user=request.user)
+                # purchase_courses = PurchaseCourse.objects.filter(user=request.user)
                 todays_date = timezone.now().date()
                 print(todays_date)
 
