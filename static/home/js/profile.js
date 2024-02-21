39e35695e7152857766c88e867596cbdc3d7aa4b
anandgadge97 <105964127+anandgadge97@users.noreply.github.com>
2024-02-21 18:54:50 +0530

    Update profile.js

--- static/home/js/profile.js
+++ static/home/js/profile.js
@@ -1,13 +1,13 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   // document.getElementById("profile-dropdown").style.display = "none";
-//   function profileButton() {
-//     if (document.getElementById("profile-dropdown").style.display != "none") {
-//       document.getElementById("profile-dropdown").style.display = "none";
-//     } else {
-//       document.getElementById("profile-dropdown").style.display = "revert";
-//     }
-//   }
-//   document
-//     .getElementById("profile-btn")
-//     .addEventListener("click", profileButton);
-// });
+document.addEventListener("DOMContentLoaded", () => {
+ document.getElementById("profile-dropdown").style.display = "none";
+   function profileButton() {
+     if (document.getElementById("profile-dropdown").style.display != "none") {
+       document.getElementById("profile-dropdown").style.display = "none";
+     } else {
+       document.getElementById("profile-dropdown").style.display = "revert";
+     }
+   }
+   document
+     .getElementById("profile-btn")
+     .addEventListener("click", profileButton);
+ });
