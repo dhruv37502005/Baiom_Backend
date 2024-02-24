226bac195fb3c5e6f0c04085624726f3bee3931f
mounikasangana0126 <sssmounika5@gmail.com>
2024-02-24 19:27:13 +0530

    I am interested button working

--- static/itie/js/script.js
+++ static/itie/js/script.js
@@ -1,9 +1,9 @@
 var carousel = document.querySelector(".carousel");
 var firstCardWidth = carousel.querySelector(".testimonials").offsetWidth;
 var arrowBtns = document.querySelectorAll(".temp i");
-var popup_form = document.getElementById("popup-form");
-var I_am_interested = document.getElementById("button-one");
-var popup_close = document.getElementById("popup-close");
+// var popup_form = document.getElementById("popup-form");
+// var I_am_interested = document.getElementById("button-one");
+// var popup_close = document.getElementById("popup-close");
 
 
 arrowBtns.forEach((btn) => {
@@ -26,14 +26,14 @@ arrowBtnsTwo.forEach((btn) => {
 
 //popup form
 
-I_am_interested.addEventListener("click", () => {
-  popup_form.style.display = "revert";
-  // document.getElementById("container").style.opacity = "0.7";
-})
+// I_am_interested.addEventListener("click", () => {
+//   popup_form.style.display = "revert";
+//   // document.getElementById("container").style.opacity = "0.7";
+// })
 
-popup_close.addEventListener("click", () => {
-  popup_form.style.display = "none";
-})
+// popup_close.addEventListener("click", () => {
+//   popup_form.style.display = "none";
+// })
 
 // popup_form.addEventListener("submit", (e) => {
 //   e.preventDefault();
