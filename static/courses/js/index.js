ce0f2c34e24adcf67a98cfc4909debd35af82eed
dhruv37502005 <149358164+dhruv37502005@users.noreply.github.com>
2024-02-28 16:52:56 +0530

    Revert "Merge branch 'mounika_new'"
    This reverts commit 91824c8c874fe3915e21ac10fc36c76cf07814b7, reversing
    changes made to 3da7dfa080e834fa31911f9e4d659765aae0cc3b.

--- static/courses/js/index.js
+++ static/courses/js/index.js
@@ -51,45 +51,15 @@
 //     });
 //   });
 
+
 var accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-
-var currHeaders = document.querySelectorAll(".curriculum-item-header");
-
-function closeAllCurr() {
-  const activeCurrHeader = document.querySelector(".curriculum-item-header.active");
-  if (activeCurrHeader) {
-    var arrow = activeCurrHeader.children[0];
-    // arrow.innerHTML="expand_more";
-    arrow.innerHTML="expand_more";
-    activeCurrHeader.classList.remove("active");
-    activeCurrHeader.nextElementSibling.style.maxHeight = 0;
-    activeCurrHeader.nextElementSibling.style.display = "none";
-    
-
-  }
-}
-
-currHeaders.forEach((currHeader) => {
-  currHeader.addEventListener("click", () => {
-    const isActive = currHeader.classList.contains("active");
-    var arrow = currHeader.children[0];
-
-    closeAllCurr();
-
-    if (!isActive) {
-      currHeader.classList.add("active");
-      arrow.innerHTML="expand_less";
-      const currBody = currHeader.nextElementSibling;
-      currBody.style.display = "block";
-      currBody.style.maxHeight = currBody.scrollHeight + "px";
-    }
-  });
-})
 
 accordionItemHeaders.forEach((accordionItemHeader) => {
   accordionItemHeader.addEventListener("click", () => {
     const isActive = accordionItemHeader.classList.contains("active");
+    
     closeAllAccordionItems();
+
     if (!isActive) {
       accordionItemHeader.classList.add("active");
       const accordionItemBody = accordionItemHeader.nextElementSibling;
@@ -109,58 +79,58 @@ function closeAllAccordionItems() {
 
 // Testimonials
 
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
+var swiper = new Swiper(".slide-content",{
+  slidesPerView:3,
   spaceBetween: 30,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
+  loop:true,
+  centerSlide:'true',
+  fade:'true',
+  grabCursor:'true',
+  pagination:{
+    el:".swiper-pagination",
+    clickable:true,
+    dynamicBullets:true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  navigation:{
+    nextEl:".swiper-button-next",
+    prevEl:".swiper-button-prev",
   },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+  breakpoints:{
+    0:{
+      slidesPerView:1,
     },
-    520: {
-      slidesPerView: 2,
+    520:{
+      slidesPerView:2,
     },
-    950: {
-      slidesPerView: 3,
+    950:{
+      slidesPerView:3,
     },
   }
 });
 
 // right course 
-var splide = new Splide('.splide', {
-  perPage: 3,
-  perMove: 1,
-  gap: '7rem',
-  padding: '1rem',
-  autoWidth: true,
-  type: 'loop',
-  drag: 'free',
-  snap: true,
-  pagination: true,
-  arrows: true,
-  breakpoints: {
-    1200: {
-      perPage: 2,
-      gap: '.7rem',
+  var splide = new Splide('.splide', {
+    perPage: 3,
+    perMove: 1,
+    gap: '7rem',
+    padding: '1rem',
+    autoWidth: true,
+    type: 'loop',
+    drag: 'free',
+    snap: true,
+    pagination: true,
+    arrows: true,
+    breakpoints: {
+      1200: {
+        perPage: 2,
+        gap: '.7rem',
+      },
+      480: {
+        perPage: 1,
+        gap: '.7rem',
+      },
     },
-    480: {
-      perPage: 1,
-      gap: '.7rem',
-    },
-  },
-}).mount();
+  }).mount();
 
 
 
