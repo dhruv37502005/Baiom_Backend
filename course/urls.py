e2c8e34ad27f7f06fc4e783d7da7bd944e37c111
SEBIN BABY <sebin.18@gmail.com>
2024-03-01 23:50:31 +0530

    course_contact view is modified

--- course/urls.py
+++ course/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('categories/<int:category_id>/', views.category_courses, name='category_courses'),
     path('categories/<int:category_id>/json', views.category_courses_json, name='category_courses_json'),
     path('download_file/<int:file_id>/', DownloadFileView.as_view(), name='download_file'),
-    path('course_contact', views.course_contact, name='course_contact')
+    path('course_contact/', views.course_contact, name='course_contact')
 
 
 
