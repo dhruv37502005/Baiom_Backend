3c7fd4730eac670f3f6a828bb02d26c131f240e3
SEBIN BABY <sebin.18@gmail.com>
2024-03-22 13:49:56 +0530

    Subscription_ITIE model and view

--- subscription/models.py
+++ subscription/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from bootcamp.models import BootCourse, BootBatch
 from course.models import Course, Batch
 import datetime
-from itie.models import ICourse
+from itie.models import IBatch, ICourse
 from userauths.models import Dashboard_User
 from django.core.exceptions import ValidationError
 
@@ -34,6 +34,7 @@ class SubscriptionPlan(models.Model):
                 return f"{self.name} - Bootcamp"
             else:
                 return f"{self.name} - {self.total_amount_after_discount}" 
+
 
 class SubscriptionPlanCourse(models.Model):
     subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, blank=True,null=True )
@@ -72,9 +73,44 @@ class SubscriptionPlanCourse(models.Model):
             end_date = self.start_date + datetime.timedelta(days=30 * self.subscription_plan.months)
             self.end_date = end_date
         super().save(*args, **kwargs)
+
+
+class SubscriptionPlanItie(models.Model):
+    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, blank=True,null=True,)
+    itie_course = models.ForeignKey(ICourse, on_delete=models.CASCADE, blank=True, null=True)
+    included_in_plan = models.BooleanField(default=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    available_slots = models.IntegerField(default=-1)  # -1 for unlimited
+
+    def __str__(self):
+        return f"{self.subscription_plan.name} - {self.itie_course.title}"
     
-   
-        
+    def clean(self):
+        super().clean()
+        if self.subscription_plan and not self.subscription_plan.is_itie:
+            raise ValidationError("Subscription plan must be for a itie.")
+       
+
+class SubscriptionPlanBootcamp(models.Model):
+    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, blank=True,null=True)
+    bootcamp_course = models.ForeignKey(BootCourse, on_delete=models.CASCADE, blank=True, null=True)
+    included_in_plan = models.BooleanField(default=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    available_slots = models.IntegerField(default=-1)  # -1 for unlimited
+
+    def __str__(self):
+        return f"{self.subscription_plan.name} - {self.bootcamp_course.title}" 
+    
+    def clean(self):
+        super().clean()
+        if self.subscription_plan and not self.subscription_plan.is_bootcamp:
+            raise ValidationError("Subscription plan must be for a bootcamp.")
+    
+      
         
 class PurchaseCourse(models.Model):
     dashboard_user = models.ForeignKey(Dashboard_User, on_delete=models.CASCADE, null=True, blank=True)
@@ -140,39 +176,37 @@ class PurchaseBootcamp(models.Model):
         super().save(*args, **kwargs)
 
 
+class PurchaseItie(models.Model):
+    dashboard_user = models.ForeignKey(Dashboard_User, on_delete=models.CASCADE, null=True, blank=True)
+    purchased_course = models.ForeignKey(ICourse, on_delete=models.CASCADE, null=True, blank=True)
+    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE,blank=True, null=True)
+    Batch = models.ForeignKey(IBatch, on_delete=models.CASCADE, null=True, blank=True)
+    purchase_date = models.DateField(auto_now_add=True)
+    plans_duration_months = models.PositiveIntegerField(editable=False)
+    purchase_start_date = models.DateField()
+    purchase_end_date = models.DateField()
+    additional_access_date = models.DateField()
 
-class SubscriptionPlanItie(models.Model):
-    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, blank=True,null=True,)
-    itie_course = models.ForeignKey(ICourse, on_delete=models.CASCADE, blank=True, null=True)
-    included_in_plan = models.BooleanField(default=True)
-    start_date = models.DateField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
-    active = models.BooleanField(default=True)
-    available_slots = models.IntegerField(default=-1)  # -1 for unlimited
-
+     
     def __str__(self):
-        return f"{self.subscription_plan.name} - {self.itie_course.title}"
-    
-    def clean(self):
-        super().clean()
-        if self.subscription_plan and not self.subscription_plan.is_itie:
-            raise ValidationError("Subscription plan must be for a itie.")
-       
+        return f"{self.purchased_course.title} - {self.subscription_plan.name} - {self.dashboard_user.fname}"
 
-class SubscriptionPlanBootcamp(models.Model):
-    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, blank=True,null=True)
-    bootcamp_course = models.ForeignKey(BootCourse, on_delete=models.CASCADE, blank=True, null=True)
-    included_in_plan = models.BooleanField(default=True)
-    start_date = models.DateField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
-    active = models.BooleanField(default=True)
-    available_slots = models.IntegerField(default=-1)  # -1 for unlimited
+    def is_course_access_valid(self):
+        now = timezone.now()
+        return self.purchase_start_date <= now <= self.purchase_end_date
 
-    def __str__(self):
-        return f"{self.subscription_plan.name} - {self.bootcamp_course.title}" 
-    
-    def clean(self):
-        super().clean()
-        if self.subscription_plan and not self.subscription_plan.is_bootcamp:
-            raise ValidationError("Subscription plan must be for a bootcamp.")
+    def can_access_recorded_videos(self):
+        now = timezone.now()
+        return self.purchase_end_date <= now <= self.additional_access_date
+
+    def save(self, *args, **kwargs):
+        # Calculate plans_duration_months based on the subscription plan
+        if self.subscription_plan:
+            self.plans_duration_months = self.subscription_plan.months
+            # Calculate purchase_end_date based on purchase_start_date and plans_duration_months
+            self.purchase_end_date = self.purchase_start_date + datetime.timedelta(days=30 * self.plans_duration_months)
+        super().save(*args, **kwargs)
+
+
+
 
